8b15e7ceeb603d2530a5d232c50f5d6657ffa8a8
MiguelVR <vilchezrodriguezm@gmail.com>
2018-09-09 17:38:19 -0500

    primer commit tarea1

---
+++ src/main/java/com/jbgroup/Taller.java
@@ -0,0 +1,11 @@
+package com.jbgroup;
+
+public class Taller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Esta es parte tarea 1");
+	}
+
+}
+ 
