1ed51aca9e348d3ddf81e02b39ed080909d9e067
MiguelVR <vilchezrodriguezm@gmail.com>
2018-09-09 18:13:53 -0500

    tercercommit

--- src/main/java/com/jbgroup/Taller.java
+++ src/main/java/com/jbgroup/Taller.java
@@ -5,7 +5,8 @@ public class Taller {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Esta es parte tarea 1");
+     System.out.println("Esta es parte tarea 2"); 
 	}
 
-}
+} 
  
